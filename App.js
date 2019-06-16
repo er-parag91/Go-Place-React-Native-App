f36d035354ce88aacc6a915c538d092d6affc307
ppatel81 <ppatel81@dxc.com>
2019-06-16 18:35:58 -0500

    plugged in redux store

--- App.js
+++ App.js
@@ -84,11 +84,15 @@ import { Navigation } from 'react-native-navigation';
 import Auth from './src/screens/Auth/Auth';
 import FindPlaceScreen from './src/screens/FindPlaceScreen/FindPlaceScreen';
 import ShareScreenPlace from './src/screens/ShareScreenPlace/ShareScreenPlace';
+import { Provider } from 'react-redux';
 
+import configureStore from './src/store/configureStore';
 
-Navigation.registerComponent("Go-places.AuthScreen", () => Auth);
-Navigation.registerComponent("Go-places.FindPlaceScreen", () => FindPlaceScreen);
-Navigation.registerComponent("Go-places.ShareScreenPlace", () => ShareScreenPlace);
+const store = configureStore();
+
+Navigation.registerComponent("Go-places.AuthScreen", () => Auth, store, Provider);
+Navigation.registerComponent("Go-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent("Go-places.ShareScreenPlace", () => ShareScreenPlace, store, Provider);
 
 Navigation.startSingleScreenApp({
   screen: {
