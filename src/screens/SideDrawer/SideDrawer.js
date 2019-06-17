36d86f9555f9601d6e9f02f4f789178f75636d24
ppatel81 <ppatel81@dxc.com>
2019-06-16 21:52:19 -0500

    added sideDrawer

---
+++ src/screens/SideDrawer/SideDrawer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SideDrawer = () => (
+    <View>
+        <Text>SideDrawer</Text>
+    </View>
+)
+
+export default SideDrawer;
