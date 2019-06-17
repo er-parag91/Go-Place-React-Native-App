46babf4858ba91fad7b042ac9e492efe2f131959
ppatel81 <ppatel81@dxc.com>
2019-06-16 20:14:53 -0500

    pushing onto the pages

--- src/screens/FindPlaceScreen/FindPlaceScreen.js
+++ src/screens/FindPlaceScreen/FindPlaceScreen.js
@@ -4,10 +4,24 @@ import { connect } from 'react-redux';
 import PlacesList from '../../PlacesList/PlacesList';
 
 class FindPlaceScreen extends Component {
+
+    itemSelectedHandler = (key) => {
+        const selectedPlace = this.props.places.find(place => {
+            return place,key === key;
+        })
+        this.props.navigator.push({
+            screen: 'Go-places.PlaceDetail',
+            title: selectedPlace.place,
+            passProps: {
+                selectedPlace: selectedPlace
+            }
+        })
+    }
+
     render(){
         return(
             <View>
-                <PlacesList places={this.props.places} />
+                <PlacesList places={this.props.places} onPlaceSelected={this.itemSelectedHandler} />
             </View>
         )
     }
