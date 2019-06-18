ffcbb6d5529341f2b29edf9d0becd0a083202d6b
ppatel81 <ppatel81@dxc.com>
2019-06-17 20:27:10 -0500

    SideDrawer Styling

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
-
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 const SideDrawer = () => (
     <View style={[styles.container, { width: Dimensions.get("window").width * 0.8 }]}>
-        <Text>SideDrawer</Text>
+        <TouchableOpacity>
+            <View style={styles.logOut}>
+                <Icon name="ios-log-out" size={30} color="#999"/>
+                <Text style={{ marginLeft: 10 }}>SideDrawer</Text>
+            </View>
+        </TouchableOpacity>
     </View>
 )
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: 22,
+        paddingTop: 50,
         backgroundColor: 'white',
         flex: 1
+    },
+    logOut: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#eee'
     }
+
 });
 
 export default SideDrawer;
