9d7299e0571c1b1ae56e3de58378c776734428ce
ppatel81 <ppatel81@dxc.com>
2019-06-17 21:33:08 -0500

    responsive design for login page with Dimension API

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, StyleSheet, ImageBackground } from 'react-native';
+import { View, Dimensions, StyleSheet, ImageBackground } from 'react-native';
 import StartMainTabs from '../MainTabs/MainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
@@ -14,18 +14,31 @@ class Auth extends Component {
     }
 
     render(){
+        let headingText = null;
+
+        if (Dimensions.get("window").height > 500) {
+            headingText = (
+                <MainText>
+                    <HeadingText>Please Log In</HeadingText>
+                </MainText>
+            );
+        }
         return(
             <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
                 <View style={styles.container}>
                     <View style={styles.login}>
-                        <MainText>
-                            <HeadingText>Please Log In</HeadingText>
-                        </MainText>
+                        {headingText}
                         <ButtonWithBackground color="#29aaf4" onPress={() => alert('Hello')}>Switch to Login</ButtonWithBackground>
                         <View style={styles.inputContainer}>
                             <DefaultInput placeholder="E-mail Address" style={styles.input} />
-                            <DefaultInput placeholder="Password" style={styles.input} />
-                            <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                            <View style={styles.passwordContainer}>
+                                <View style={styles.passwordWrapper}>
+                                    <DefaultInput placeholder="Password" style={styles.input} />
+                                </View>
+                                <View style={styles.passwordWrapper}>
+                                    <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                                </View>
+                            </View>
                         </View>
                         <ButtonWithBackground onPress={this.LoginHandler} color="#29aaf4">Login</ButtonWithBackground>
                     </View>
@@ -49,6 +62,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#00000066',
         width: '90%',
         alignItems: 'center',
+        paddingTop: 25,
+        paddingBottom: 25
     },
     inputContainer: {
         width: '80%'
@@ -56,6 +71,14 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: '#eee',
         borderColor: '#bbb'
+    },
+    passwordContainer: {
+        width: "100%",
+        flexDirection: Dimensions.get("window").height > 500 ? 'column' : 'row',
+        justifyContent: 'space-between'
+    },
+    passwordWrapper: {
+        width: Dimensions.get("window").height > 500 ? '100%' : '48%'
     }
 });
 
--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+
 const SideDrawer = () => (
     <View style={[styles.container, { width: Dimensions.get("window").width * 0.8 }]}>
         <TouchableOpacity>
             <View style={styles.logOut}>
-                <Icon name="ios-log-out" size={30} color="#999"/>
+                <Icon 
+                    name={Platform.OS === "android" ? "md-log-out"  : "ios-log-out"}
+                    size={30}
+                    color="#999"
+                    />
                 <Text style={{ marginLeft: 10 }}>SideDrawer</Text>
             </View>
         </TouchableOpacity>
