1210ef8b56d4af1d0424761c485d9b67a8a5b3bd
ppatel81 <ppatel81@dxc.com>
2019-06-19 15:02:01 -0500

    installed and linked react native image picker package

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -1,16 +1,81 @@
 import React, { Component } from 'react';
-import { View , Image, Button, StyleSheet} from 'react-native';
+import { View, Image, Button, StyleSheet } from 'react-native';
 import imagePlaceHolder from '../../Assets/imagePlaceholder.jpg';
+import ImagePicker from 'react-native-image-picker';
 
-class PickImage extends Component{
-    render(){
-        return(
+const options = {
+    title: 'Select Photo',
+    customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images',
+    },
+};
+
+class PickImage extends Component {
+
+    state = {
+        avatarSource: imagePlaceHolder
+    }
+
+    pickImageHandler = () => {
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                const source = { uri: response.uri };
+
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                this.setState({
+                    avatarSource: source,
+                });
+            }
+        });
+    }
+
+
+    // // Launch Camera:
+    // ImagePicker.launchCamera(options, (response) => {
+    //     console.log('Response = ', response);
+
+    //         if (response.didCancel) {
+    //             console.log('User cancelled image picker');
+    //         } else if (response.error) {
+    //             console.log('ImagePicker Error: ', response.error);
+    //         } else if (response.customButton) {
+    //             console.log('User tapped custom button: ', response.customButton);
+    //         } else {
+    //             const source = { uri: response.uri };
+
+    //             // You can also display the image using data:
+    //             // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+    //             this.setState({
+    //                 avatarSource: source,
+    //             });
+    //         }
+    // });
+
+    // // Open Image Library:
+    // ImagePicker.launchImageLibrary(options, (response) => {
+    //     // Same code as in above section!
+    // });
+    render() {
+        return (
             <View style={styles.container}>
                 <View style={styles.placeholder}>
-                    <Image source={imagePlaceHolder} style={styles.imagePlaceHolder} />
+                    <Image source={this.state.avatarSource} style={styles.imagePlaceHolder} />
                 </View>
                 <View style={styles.button}>
-                    <Button title="Select Image" />
+                    <Button title="Select Image" onPress={this.pickImageHandler} />
                 </View>
             </View>
         );
