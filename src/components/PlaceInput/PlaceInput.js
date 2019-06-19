f1662424a77d20e9707a0b32846a90a45cbb62cd
ppatel81 <ppatel81@dxc.com>
2019-06-18 21:45:47 -0500

    share place screen input validation and installed react-native-map library

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -4,7 +4,9 @@ import DefaultInput from '../UI/DefaultInput/DefaultInput';
 const PlaceInput = props => (
     <DefaultInput
         placeholder="Place Name"
-        value={props.placeName}
+        value={props.placeName.value}
+        valid = {!props.placeName.valid}
+        touched={props.placeName.touched}
         onChangeText={props.onChangeHandler}
     />
 );
