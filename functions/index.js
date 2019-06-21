956a96c5eeb45382e57465be1c90f7aff4680713
ppatel81 <ppatel81@dxc.com>
2019-06-21 15:38:49 -0500

    modified cloud funtion to extract and verify the token

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const cors = require("cors")({
 });
 const fs = require("fs");
 const UUID = require("uuidv4");
-
+const admin = require("firebase-admin");
 
 const {
     Storage
@@ -13,44 +13,65 @@ const storage = new Storage({
     projectId: "go-places-79741"
 })
 
+admin.initializeApp({
+    credential: admin.credential.cert(require("./go-places.json"))
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeImage = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
-        const body = JSON.parse(request.body);
-        fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
-            return response.status(500).json({
-                error: err
+        if (!request.headers.authorization || !request.headers.authorization.startsWith('Bearer ')) {
+            response.status(403).json({
+                error: "Unauthorized"
             });
-        });
-        const bucket = storage.bucket("go-places-79741.appspot.com");
-        const uuid = UUID();
-        bucket.upload("/tmp/uploaded-image.jpg", {
-            uploadType: "media",
-            destination: "/places/" + uuid + ".jpg",
-            metadata: {
-                metadata: {
-                    contentType: "image/jpeg",
-                    firebaseStorageDownloadTokens: uuid
-                }
-            }
+            return;
+        }
+        let idToken;
+        idToken = request.headers.authorization.split('Bearer ')[1];
+        admin
+            .auth()
+            .verifyIdToken(idToken)
+            .then(decodedToken => {
+                const body = JSON.parse(request.body);
+                fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
+                    return response.status(500).json({
+                        error: err
+                    });
+                });
+                const bucket = storage.bucket("go-places-79741.appspot.com");
+                const uuid = UUID();
+                bucket.upload("/tmp/uploaded-image.jpg", {
+                    uploadType: "media",
+                    destination: "/places/" + uuid + ".jpg",
+                    metadata: {
+                        metadata: {
+                            contentType: "image/jpeg",
+                            firebaseStorageDownloadTokens: uuid
+                        }
+                    }
 
-        }, (err, file) => {
-            if (!err) {
-                response.status(201).json({
-                    imageUrl: "https://firebasestorage.googleapis.com/v0/b/" +
-                        bucket.name +
-                        "/o/" +
-                        encodeURIComponent(file.name) +
-                        "?alt=media&token=" +
-                        uuid
+                }, (err, file) => {
+                    if (!err) {
+                        response.status(201).json({
+                            imageUrl: "https://firebasestorage.googleapis.com/v0/b/" +
+                                bucket.name +
+                                "/o/" +
+                                encodeURIComponent(file.name) +
+                                "?alt=media&token=" +
+                                uuid
+                        });
+                    } else {
+                        response.status(500).json({
+                            error: err
+                        });
+                    }
                 });
-            } else {
-                response.status(500).json({
-                    error: err
+            })
+            .catch(() => {
+                response.status(403).json({
+                    error: "Unauthorized"
                 });
-            }
-        });
+            });
     });
 });
